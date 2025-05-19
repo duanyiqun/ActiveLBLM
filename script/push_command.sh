7f67559b6ae3fb9d8102fd2d2b6a4160e8cffc28
unknown <IDOMathEveryDay>
2025-05-19 15:27:54 +1000

    formal

---
+++ script/push_command.sh
@@ -0,0 +1,9 @@
+# via git bash
+cd ActiveLBLM/
+git checkout master # make sure you are on the master branch
+git add .
+git commit -m "test_push"
+git push --set-upstream origin master
+
+
+# the static page can be run by chrome. 
